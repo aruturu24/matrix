4305ea2d9affb4e02f7892c6fd778604220a6375
aruturu24 <aruturu24@hotmail.com>
2022-06-26 17:56:02 -0300

    + System to change text and color

--- script.js
+++ script.js
@@ -11,15 +11,19 @@ var font_size = 20;
 var cols = Math.floor(c.width / font_size) + 1;
 var ypos = Array(cols).fill(0);
 
+var font_color = "#0F0";
+var random_char = true;
+
 function draw() {
   ctx.fillStyle = "#0001";
   ctx.fillRect(0, 0, c.width, c.height);
 
-  ctx.fillStyle = "#0F0";
+  ctx.fillStyle = font_color;
   ctx.font = font_size + "px arial";
 
   ypos.forEach((y, i) => {
-    const txt = characters[Math.floor(Math.random() * characters.length)];
+
+    const txt = random_char ? characters[Math.floor(Math.random() * characters.length)] : characters[i % characters.length];
     const x = i * font_size;
     ctx.fillText(txt, x, y);
     
@@ -29,6 +33,17 @@ function draw() {
       ypos[i] = y + font_size;
     }
   });
+}
+
+c.addEventListener("click", (e) => {
+  document.getElementById("options").classList.toggle("hide");
+});
+
+function saveChanges() {
+  font_color = document.getElementById("color").value;
+  characters = document.getElementById("chars").value || characters.join("");
+  characters = characters.split("");
+  random_char = document.getElementById("rand").checked;
 }
 
 setInterval(draw, 75);
